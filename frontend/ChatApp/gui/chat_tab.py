6e7302da7427009dbfc422d68555bfbc4936d5e8
thstein7 <31679672+thstein7@users.noreply.github.com>
2019-11-02 12:55:57 -0400

    add scrolling

--- frontend/ChatApp/gui/chat_tab.py
+++ frontend/ChatApp/gui/chat_tab.py
@@ -9,17 +9,18 @@ class ChatTab(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         
-        self._layout = None
+        self.main_layout = None
         self.message_enter = None
         self.send_button = None
         self.messages_layout = None
+        self.scroll = None
         self.message_widgets = list()
 
         self._init_ui()
 
     def _init_ui(self):
-        self._layout = QtWidgets.QVBoxLayout()
-        self.messages_layout = QtWidgets.QVBoxLayout()
+        self.main_layout = QtWidgets.QVBoxLayout()
+        self.messages_layout = QtWidgets.QFormLayout()
 
         send_layout = QtWidgets.QVBoxLayout()
         self.message_enter = QtWidgets.QLineEdit() # TODO may need plaintext edit?
@@ -28,15 +29,25 @@ class ChatTab(QtWidgets.QWidget):
         send_layout.addWidget(self.message_enter)
         send_layout.addWidget(self.send_button)
 
-        self._layout.addLayout(self.messages_layout)
-        self._layout.addLayout(send_layout)
 
-        self.setLayout(self._layout)
+        self.scroll = QtWidgets.QScrollArea()
+        self.scroll.setWidgetResizable(True)
+        widget = QtWidgets.QWidget(self)
+        widget.setLayout(self.messages_layout)
+        self.scroll.setWidget(widget)
+        self.main_layout.addWidget(self.scroll)
+        self.main_layout.addLayout(send_layout)
+        self.setLayout(self.main_layout)
+
+        # Add seed messages
+        for i in range(100):
+            self.message_enter.setText(str(i))
+            self._send_clicked()
 
     def _send_clicked(self):
         print('send clicked')
         time_str = datetime.now().strftime('%m/%d/%y %I:%M %p')
         msg_widget = MessageWidget(self.message_enter.text(), time_str, parent=self)
         self.message_widgets.append(msg_widget)
-        self.messages_layout.addWidget(msg_widget)
+        self.messages_layout.addRow(msg_widget)
         self.message_enter.setText('')
